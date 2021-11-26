4b49f9141f244372e1d0be14960297b994371660
Azkadev <azkadev@pop-os.localdomain>
2021-11-26 11:20:09 +0700

    add scroll vertical and horizontal

--- lib/main.dart
+++ lib/main.dart
@@ -38,168 +38,202 @@ class _ExpensesPageState extends State<MainPage> {
       backgroundColor: const Color(0xffF0F8FF),
       body: SafeArea(
         minimum: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                InkWell(
-                  child: const CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        'https://avatars.githubusercontent.com/u/82513502?v=4'),
-                  ),
-                  onTap: () {
-                    // ignore: avoid_print
-                    print("tap pp");
-                  },
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                const Text(
-                  "Gibran Alazka",
-                  style: TextStyle(
-                    color: Colors.blueGrey,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const Spacer(),
-                InkWell(
-                  child: Container(
-                    width: 30,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      color: const Color(0xff161B22),
-                      borderRadius: BorderRadius.circular(10),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  InkWell(
+                    child: const CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://avatars.githubusercontent.com/u/82513502?v=4'),
                     ),
-                    child: const Center(
-                      child: Icon(
-                        Icons.add,
-                        color: Colors.white,
-                        size: 25,
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("tap pp");
+                    },
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  const Text(
+                    "Gibran Alazka",
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const Spacer(),
+                  InkWell(
+                    child: Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: const Color(0xff161B22),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 25,
+                        ),
                       ),
                     ),
+                    onTap: () {
+                      // ignore: avoid_print
+                      print("tap pp");
+                    },
                   ),
-                  onTap: () {
-                    // ignore: avoid_print
-                    print("tap pp");
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            InkWell(
-              borderRadius: BorderRadius.circular(20),
-              child: Container(
-                width: getWidht,
-                height: 180,
-                decoration: BoxDecoration(
-                  color: const Color(0xffFFFFFF),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: SafeArea(
-                  minimum: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          const Text(
-                            "Total Wallet Balance",
-                            style: TextStyle(
-                              color: Colors.blueGrey,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          const Spacer(),
-                          InkWell(
-                            child: const Text(
-                              "USD",
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              InkWell(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  width: getWidht,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    color: const Color(0xffFFFFFF),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: SafeArea(
+                    minimum: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const Text(
+                              "Total Wallet Balance",
                               style: TextStyle(
-                                color: Color(0xFFF7931A),
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
+                                color: Colors.blueGrey,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
                               ),
                             ),
-                            onTap: () {},
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        "\$ 66641.20",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 15),
-                      const Text(
-                        "Monthly Profit",
-                        style: TextStyle(
-                          color: Colors.blueGrey,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      const SizedBox(height: 2),
-                      Row(
-                        children: [
-                          const Text(
-                            "\$1850.00",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 25,
-                            ),
-                          ),
-                          const Spacer(),
-                          Container(
-                            constraints: const BoxConstraints(
-                              maxHeight: double.infinity,
-                            ),
-                            height: 30,
-                            decoration: BoxDecoration(
-                              color: const Color(0xff58BD7D),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Icon(
-                                  Icons.add,
-                                  color: Colors.white,
-                                  size: 15,
+                            const Spacer(),
+                            InkWell(
+                              child: const Text(
+                                "USD",
+                                style: TextStyle(
+                                  color: Color(0xFFF7931A),
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                Text(" +13%")
-                              ],
+                              ),
+                              onTap: () {},
                             ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        const Text(
+                          "\$ 66641.20",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        const SizedBox(height: 15),
+                        const Text(
+                          "Monthly Profit",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        const SizedBox(height: 2),
+                        Row(
+                          children: [
+                            const Text(
+                              "\$1850.00",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 25,
+                              ),
+                            ),
+                            const Spacer(),
+                            Container(
+                              constraints: const BoxConstraints(
+                                maxHeight: double.infinity,
+                              ),
+                              height: 30,
+                              decoration: BoxDecoration(
+                                color: const Color(0xff58BD7D),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [
+                                  Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                    size: 15,
+                                  ),
+                                  Text(" +13%")
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+                onTap: () {},
               ),
-              onTap: () {},
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              "Portofolio",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
+              const SizedBox(height: 20),
+              const Text(
+                "Portofolio",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-          ],
+              const SizedBox(height: 20),
+              SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: const [
+                      Text("sasasas"),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text("bbjbj"),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text("sasasas"),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text("bbjbj"),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text("bbjbj"),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text("sasasas"),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text("bbjbj"),
+                    ],
+                  ))
+            ],
+          ),
         ),
       ),
     );
